cd0c705619fe13b83fab308d34d963cb202b3337
MarojT <celta0992@hotmail.com>
2024-04-23 04:33:41 +0700

    make ball spawn in random interval.

--- Assets/Challenge 2/Scripts/SpawnManagerX.cs
+++ Assets/Challenge 2/Scripts/SpawnManagerX.cs
@@ -11,33 +11,28 @@ public class SpawnManagerX : MonoBehaviour
     private float spawnPosY = 30;
 
     private float startDelay = 1.0f;
-    private List<int> spawnInterval;
     private int rndBall;
-    private float rndInterval;
+    private float interval;
 
     // Start is called before the first frame update
 
     void Start()
     {
-        InvokeRepeating("SpawnRandomBall", startDelay, RandomInterval());
+        InvokeRepeating("SpawnRandomBall", startDelay, interval);
     }
 
     // Spawn random ball at random x position at top of play area
     void SpawnRandomBall ()
     {
+        interval = Random.Range(3, 5);
         // Generate random ball index and random spawn position
         Vector3 spawnPos = new Vector3(Random.Range(spawnLimitXLeft, spawnLimitXRight), spawnPosY, 0);
 
         // instantiate ball at random spawn location
         rndBall = Random.Range(0, ballPrefabs.Length);
         Instantiate(ballPrefabs[rndBall], spawnPos, ballPrefabs[rndBall].transform.rotation);
-    }
 
-    float RandomInterval()
-    {
-        float rndInterval = Random.Range(3, 5);
-        Debug.Log(rndInterval);
-        return rndInterval;
+        //recursive this function to make interval chang after spawn balls
+        Invoke("SpawnRandomBall", interval);
     }
-
 }
